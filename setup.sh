bc42c293ce082dbb4f042f7567e830f51295c6f5
Hilco Wijbenga <hilco.wijbenga@gmail.com>
2014-12-13 19:12:17 -0800

    Add set up of /etc/hosts.

--- setup.sh
+++ setup.sh
@@ -12,3 +12,9 @@ useradd -d /etc/ansible -m -r -s /bin/bash ansible
 rm -rf /etc/ansible/.??* /etc/ansible/*
 
 su - ansible -c "git clone https://github.com/Hilco-Wijbenga/ansible.git ."
+
+cat >/etc/hosts <<EOF
+127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4
+::1         localhost localhost.localdomain localhost6 localhost6.localdomain6
+10.0.2.15   devbox    devbox.cavebeetle.org
+EOF
